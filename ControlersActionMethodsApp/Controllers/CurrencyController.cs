91fbfa5b468b9b52522640f45cf3225f83565bb3
Alireza <119754279+Sheikhly@users.noreply.github.com>
2022-12-06 20:50:04 +0330

    Update
    All the links Added

---
+++ ControlersActionMethodsApp/Controllers/CurrencyController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ControlersActionMethodsApp.Models;
+
+namespace ControlersActionMethodsApp.Controllers
+{
+    public class CurrencyController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult BTC()
+        {
+            return View();
+        }
+
+
+        public IActionResult ETH()
+        {
+            return View();
+        }
+        
+
+
+    }
+}
+
+
+
+
